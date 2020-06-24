ad2972714dd250a99dd9d2eded232d873f972226
TXT-54 <txt-54@macbook-pro-5.attlocal.net>
2020-06-23 21:24:27 -0700

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+function ngModel (){
+
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'firstapp';
+  name="name";
+  lastName="lastname";
+}
